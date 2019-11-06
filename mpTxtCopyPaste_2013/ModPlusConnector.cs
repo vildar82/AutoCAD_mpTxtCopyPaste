5f73fd99098967e6592ea397122e84d384e2d491
Modis <cjmodis@gmail.com>
2019-11-06 21:15:47 +0300

    Add stylecop. CleanUp

--- mpTxtCopyPaste_2013/ModPlusConnector.cs
+++ mpTxtCopyPaste_2013/ModPlusConnector.cs
@@ -1,63 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using ModPlusAPI.Interfaces;
-
-namespace mpTxtCopyPaste
+﻿namespace mpTxtCopyPaste
 {
+    using System;
+    using System.Collections.Generic;
+    using ModPlusAPI.Interfaces;
+
+    /// <inheritdoc />
     public class ModPlusConnector : IModPlusFunctionInterface
     {
+        /// <inheritdoc />
         public SupportedProduct SupportedProduct => SupportedProduct.AutoCAD;
         
+        /// <inheritdoc />
         public string Name => "mpTxtCopyPaste";
 
 #if A2013
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2013";
 #elif A2014
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2014";
 #elif A2015
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2015";
 #elif A2016
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2016";
 #elif A2017
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2017";
 #elif A2018
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2018";
 #elif A2019
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2019";
 #elif A2020
+        /// <inheritdoc />
         public string AvailProductExternalVersion => "2020";
 #endif
 
+        /// <inheritdoc />
         public string FullClassName => string.Empty;
         
+        /// <inheritdoc />
         public string AppFullClassName => string.Empty;
         
+        /// <inheritdoc />
         public Guid AddInId => Guid.Empty;
         
+        /// <inheritdoc />
         public string LName => "Быстрая копия текста";
         
+        /// <inheritdoc />
         public string Description => "Быстрое копирование содержимого однострочного или многострочного текста";
         
+        /// <inheritdoc />
         public string Author => "Пекшев Александр aka Modis";
         
+        /// <inheritdoc />
         public string Price => "0";
         
+        /// <inheritdoc />
         public bool CanAddToRibbon => true;
         
+        /// <inheritdoc />
         public string FullDescription => string.Empty;
         
+        /// <inheritdoc />
         public string ToolTipHelpImage => string.Empty;
         
+        /// <inheritdoc />
         public List<string> SubFunctionsNames => new List<string>();
         
+        /// <inheritdoc />
         public List<string> SubFunctionsLames => new List<string>();
         
+        /// <inheritdoc />
         public List<string> SubDescriptions => new List<string>();
         
+        /// <inheritdoc />
         public List<string> SubFullDescriptions => new List<string>();
         
+        /// <inheritdoc />
         public List<string> SubHelpImages => new List<string>();
         
+        /// <inheritdoc />
         public List<string> SubClassNames => new List<string>();
     }
 }
--- mpTxtCopyPaste_2013/mpTxtCopyPaste.cs
+++ mpTxtCopyPaste_2013/mpTxtCopyPaste.cs
@@ -1,16 +1,22 @@
-﻿using AcApp = Autodesk.AutoCAD.ApplicationServices.Core.Application;
-using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Runtime;
-using ModPlusAPI;
-using ModPlusAPI.Windows;
-
-namespace mpTxtCopyPaste
+﻿namespace mpTxtCopyPaste
 {
+    using Autodesk.AutoCAD.DatabaseServices;
+    using Autodesk.AutoCAD.EditorInput;
+    using Autodesk.AutoCAD.Runtime;
+    using ModPlusAPI;
+    using ModPlusAPI.Windows;
+    using AcApp = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+    /// <summary>
+    /// Main class of plugin
+    /// </summary>
     public class MpTxtCopyPaste
     {
         private const string LangItem = "mpTxtCopyPaste";
 
+        /// <summary>
+        /// Command start
+        /// </summary>
         [CommandMethod("ModPlus", "mpTxtCopyPaste", CommandFlags.UsePickSet)]
         public static void MainFunction()
         {
@@ -19,6 +25,7 @@ namespace mpTxtCopyPaste
             try
             {
                 var deleteSource = false; // Удаление исходника
+
                 // Значение по-умолчанию из расширенных данных чертеже
                 var defVal = ModPlus.Helpers.XDataHelpers.GetStringXData("mpTxtCopyPaste");
                 if (!string.IsNullOrEmpty(defVal))
@@ -26,7 +33,7 @@ namespace mpTxtCopyPaste
                     if (bool.TryParse(defVal, out var tmp))
                         deleteSource = tmp;
                 }
-                // 
+
                 var doc = AcApp.DocumentManager.MdiActiveDocument;
                 var db = doc.Database;
                 var ed = doc.Editor;
@@ -42,6 +49,7 @@ namespace mpTxtCopyPaste
                 if (per.Status == PromptStatus.Keyword)
                 {
                     deleteSource = MessageBox.ShowYesNo(Language.GetItem(LangItem, "msg4"), MessageBoxIcon.Question);
+
                     // Сохраняем текущее значение как значение по умолчанию
                     using (doc.LockDocument())
                     {
@@ -56,18 +64,19 @@ namespace mpTxtCopyPaste
                         {
                             var ent = (Entity)tr.GetObject(per.ObjectId, OpenMode.ForWrite);
                             var str = string.Empty;
+
                             // Если выбранный примитив - однострочный текст
-                            if (ent is DBText)
+                            if (ent is DBText dbText)
                             {
-                                var txt = (DBText)ent;
-                                str = txt.TextString;
+                                str = dbText.TextString;
                             }
+
                             // Если выбранный примитив - многострочный текст
-                            else if (ent is MText)
+                            else if (ent is MText mText)
                             {
-                                var txt = (MText)ent;
-                                str = txt.Contents;
+                                str = mText.Contents;
                             }
+
                             while (true)
                             {
                                 peo = new PromptEntityOptions("\n" + Language.GetItem(LangItem, "msg5"));
@@ -79,22 +88,23 @@ namespace mpTxtCopyPaste
                                 if (per.Status != PromptStatus.OK)
                                     break;
                                 var selectedEnt = (Entity)tr.GetObject(per.ObjectId, OpenMode.ForWrite);
-                                if (selectedEnt is DBText)
+                                if (selectedEnt is DBText selectedDbText)
                                 {
-                                    var selectedText = (DBText)selectedEnt;
-                                    selectedText.UpgradeOpen();
-                                    selectedText.TextString = str;
-                                    selectedText.DowngradeOpen();
+                                    selectedDbText.UpgradeOpen();
+                                    selectedDbText.TextString = str;
+                                    selectedDbText.DowngradeOpen();
                                 }
-                                if (selectedEnt is MText)
+
+                                if (selectedEnt is MText selectedMText)
                                 {
-                                    var selectedText = (MText)selectedEnt;
-                                    selectedText.UpgradeOpen();
-                                    selectedText.Contents = str;
-                                    selectedText.DowngradeOpen();
+                                    selectedMText.UpgradeOpen();
+                                    selectedMText.Contents = str;
+                                    selectedMText.DowngradeOpen();
                                 }
+
                                 db.TransactionManager.QueueForGraphicsFlush();
                             }
+
                             // Delete source
                             if (deleteSource)
                                 ent.Erase(true);
